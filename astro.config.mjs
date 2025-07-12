f6101e6b3bab2e7c739a088a06ee98a9c7b326f4
Bjorn Harvold <bjorn@harvold.com>
2025-07-12 13:06:33 +0700

    docs: :chart_with_upwards_trend: added GA4 Measurement ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fysioterapi25.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    icon(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
